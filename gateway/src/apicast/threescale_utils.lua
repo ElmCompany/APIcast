930c324869e872b163705c91021a00b2a7b66a7f
Abdullah Barrak <a@abarrak.com>
2022-02-10 12:05:00 +0300

    1316: support resilient redis mode for cache utils
    Tests passing:
    ```
    $ make busted-util
    /usr/local/openresty/luajit/bin/rover exec bin/busted "spec/threescale_utils_spec.lua"
    ●●●
    3 successes / 0 failures / 0 errors / 0 pending : 0.02537 seconds
    ```

--- gateway/src/apicast/threescale_utils.lua
+++ gateway/src/apicast/threescale_utils.lua
@@ -124,7 +124,7 @@ function _M.connect_redis(options)
   local opts = {}
 
   local url = options and options.url or env.get('REDIS_URL')
-
+  local resilient = options.resilient
 
   if url then
     url = resty_url.split(url, 'redis')
@@ -152,14 +152,22 @@ function _M.connect_redis(options)
 
   local ok, err = red:connect(_M.resolve(host, port))
   if not ok then
-    return nil, _M.error("failed to connect to redis on ", host, ":", port, ": ", err)
+    if not resilient then
+      return nil, _M.error("failed to connect to redis on ", host, ":", port, ": ", err)
+    else
+      return nil, _M.error_silently("failed to connect to redis on ", host, ":", port, ": ", err)
+    end
   end
 
   if opts.password then
     ok = red:auth(opts.password)
 
     if not ok then
-      return nil, _M.error("failed to auth on redis ", host, ":", port)
+      if not resilient then
+        return nil, _M.error("failed to auth on redis ", host, ":", port)
+      else
+        return nil, _M.error_silently("failed to auth on redis ", host, ":", port)
+      end
     end
   end
 
@@ -167,7 +175,11 @@ function _M.connect_redis(options)
     ok = red:select(opts.db)
 
     if not ok then
-      return nil, _M.error("failed to select db ", opts.db, " on redis ", host, ":", port)
+      if not resilient then
+        return nil, _M.error("failed to select db ", opts.db, " on redis ", host, ":", port)
+      else
+        return nil, _M.error_silently("failed to select db ", opts.db, " on redis ", host, ":", port)
+      end
     end
   end
 
@@ -187,15 +199,23 @@ function _M.match_xml_element(xml, element, value)
   return string.find(xml, pattern, xml_header_len, xml_header_len, true)
 end
 
--- error and exit
-function _M.error(...)
+-- error without exit
+function _M.error_silently(...)
   if ngx.get_phase() == 'timer' then
     return table.concat(table.pack(...))
   else
     ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
     ngx.say(...)
-    ngx.exit(ngx.status)
   end
+end
+
+-- error and exit
+function _M.error(...)
+  local err = _M.error_silently(...)
+  if err then
+    return err
+  end
+  ngx.exit(ngx.status)
 end
 
 function _M.missing_args(text)
--- spec/threescale_utils_spec.lua
+++ spec/threescale_utils_spec.lua
@@ -10,5 +10,27 @@ describe('3scale utils', function()
 
             assert.equal('one two three', error)
         end)
+
+        it('.error fails the nginx chain', function()
+            stub(ngx, 'get_phase', function() return 'init' end)
+            stub(ngx, 'say', function(...) return nil end)
+            local exit = spy.on(ngx, 'exit', function(s) return 'exited!' end)
+
+            local error = _M.error('cache issue ' .. 'host:' .. 6379)
+
+            assert.spy(ngx.exit).was_called(1)
+            assert.spy(ngx.say).was.called_with('cache issue ' .. 'host:' .. 6379)
+        end)
+
+        it('.error_silently logs the error without exiting chain', function()
+            stub(ngx, 'get_phase', function() return 'init' end)
+            stub(ngx, 'say', function(...) return nil end)
+
+            local exit = spy.on(ngx, 'exit', function(s) return 'exited!' end)
+            local error = _M.error_silently('redis is not reachable')
+
+            assert.spy(ngx.exit).was_not_called()
+            assert.spy(ngx.say).was.called_with('redis is not reachable')
+        end)
     end)
 end)
